6838d5fb86e5c0c6025ab35a9f5ad1eb505dfd9d
masakuni <maito@gmo-c.jp>
2017-02-23 18:02:18 +0900

    first commit

---
+++ touch_twitter.rb
@@ -0,0 +1,10 @@
+require "twitter"
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "YOUR_CONSUMER_KEY"
+  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+  config.access_token        = "YOUR_ACCESS_TOKEN"
+  config.access_token_secret = "YOUR_ACCESS_SECRET"
+end
+
+client.update("聞こえるかー！　アスナー！　俺だー！")
